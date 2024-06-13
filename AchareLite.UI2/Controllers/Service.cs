533a82da86f59db7a40a37e085e807fae6e3917e
Mohammad <m.ganjikh2020@gmail.com>
2024-06-13 16:02:34 +0430

    Service CRUD - Controller + UI

--- AchareLite.UI2/Controllers/Service.cs
+++ AchareLite.UI2/Controllers/Service.cs
@@ -18,10 +18,7 @@ namespace AchareLite.UI2.Controllers
             ViewData["Services"] = serviceDtos;
             return View();
         }
-        public async Task<IActionResult> Create()
-        {
-            return View();
-        }
+        
         public async Task<IActionResult> ShowListOfServicesWithSubcategoryId(CancellationToken cancellationToken, int id =0)
         {
             if (id > 0)
@@ -31,6 +28,32 @@ namespace AchareLite.UI2.Controllers
                 TempData["subCategoryId"] = id;
             }
             return View();
+        }
+        public async Task<IActionResult> Create()
+        {
+            ViewData["subCategoryId"] = (int)TempData["subCategoryId"];
+            return View();
+        }
+        public async Task<IActionResult> CreateConfirm(ServiceDto serviceDto, CancellationToken cancellationToken)
+        {
+            await _serviceAppService.Create(serviceDto.Title, serviceDto.SubCategoryId, cancellationToken);
+            return RedirectToAction("ShowListOfServicesWithSubcategoryId", "Service", new { cancellationToken, id = serviceDto.SubCategoryId });
+        }
+        public async Task<IActionResult> Delete(int id, CancellationToken cancellationToken)
+        {
+            await _serviceAppService.Delete(id, cancellationToken);
+            return RedirectToAction("ShowListOfServicesWithSubcategoryId", "Service");
+        }
+        public async Task<IActionResult> Edit(int id , CancellationToken cancellationToken)
+        {
+            (ServiceDto?, bool) serviceDto = await _serviceAppService.GetById(id, cancellationToken);
+            ViewData["serviceDto"] = serviceDto.Item1;
+            return View();
+        }
+        public async Task<IActionResult> Update(ServiceDto serviceDto,CancellationToken cancellationToken)
+        {
+            await _serviceAppService.Update(serviceDto, cancellationToken);
+            return RedirectToAction("ShowListOfServicesWithSubcategoryId", "Service", new { cancellationToken, id = serviceDto.SubCategoryId });
         }
     }
 }
